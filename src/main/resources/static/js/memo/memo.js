284b33822af983b5fda47b0d99cd4fadb570f2e8
himdol <ihimchan746@gmail.com>
2022-09-09 08:02:09 +0900

    index 페이지 한글 없애기 js todolist 작성

--- src/main/resources/static/js/memo/memo.js
+++ src/main/resources/static/js/memo/memo.js
@@ -17,16 +17,25 @@ let bind = (function() {
     return {
         tableDataBind : function () {
             tableTitle();
-            // tableContents();
-            let tr = __memoTable.insertRow(1);
-            let cell1 = tr.insertCell(0);
-            cell1.innerText = "test";
+            tableContents();
         }
     };
 }());
 
 function tableTitle () {
     let tr = __memoTable.insertRow();
-
     tr.insertCell(0).innerText = "게시글 번호";
+    tr.insertCell(1).innerText = "게시글 제목";
+    tr.insertCell(2).innerText = "게시글 내용";
+    tr.insertCell(3).innerText = "게시자";
+    tr.insertCell(4).innerText = "생성일";
+    tr.insertCell(5).innerText = "생성자";
+    tr.insertCell(6).innerText = "수정일";
+    tr.insertCell(7).innerText = "수정자";
 }
+
+function tableContents () {
+    //todo : 테이블 컨텐츠 만들기
+}
+
+//todo : 제목클릭시 상세페이지로 이동
