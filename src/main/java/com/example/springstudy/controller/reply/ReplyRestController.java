8fd136a454cc4b8fda4b19533e4b7e37032ddb9b
himdol <ihimchan746@gmail.com>
2022-09-15 21:40:29 +0900

    댓글 테스트 중

--- src/main/java/com/example/springstudy/controller/reply/ReplyRestController.java
+++ src/main/java/com/example/springstudy/controller/reply/ReplyRestController.java
@@ -3,7 +3,6 @@ package com.example.springstudy.controller.reply;
 import com.example.springstudy.dto.ReplyDto;
 import com.example.springstudy.service.reply.ReplyService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +17,7 @@ public class ReplyRestController {
     }
 
     @GetMapping("/save")
-    public ReplyDto save(@RequestBody ReplyDto replyDto) {
+    public ReplyDto save() {
         replyService.testSelectNumberOne(1);
         return null;
     }
--- src/main/java/com/example/springstudy/repository/reply/ReplyRepository.java
+++ src/main/java/com/example/springstudy/repository/reply/ReplyRepository.java
@@ -6,8 +6,15 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface ReplyRepository extends JpaRepository<ReplyEntity, Integer> {
 
-    @Query("SELECT a.SEQ from REPLY a where a.SEQ =: seq")
+    @Query(value = "SELECT a.SEQ from REPLY a where a.SEQ = seq",nativeQuery = true)
     ReplyEntity testSelectNumberOne(int seq);
+
+
+//    @Query(value = "select * from Account where DATETIMESTAMP >= :dateFrom  AND DATETIMESTAMP < :DATE_FORMAT(curdate(), '%d/%m/%Y')", nativeQuery = true)
+//    List<Account> findByDate(@Param("dateFrom") String dateFrom);
+//
+//    @Query(value = "select * from Account where DATETIMESTAMP >= :dateFrom  AND DATETIMESTAMP < DATE_FORMAT(curdate(), '%d/%m/%Y')", nativeQuery = true)
+//    List<Account> findByDate(@Param("dateFrom") String dateFrom);
 
 
 }
